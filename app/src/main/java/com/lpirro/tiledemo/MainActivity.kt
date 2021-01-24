e1b59b792cec60e1d7dcd7e0ef84c6aff2ef39e6
amit.kumar <amit.ap1924@gmail.com>
2021-01-24 08:50:56 +0530

    added system overlay

--- app/src/main/java/com/lpirro/tiledemo/MainActivity.kt
+++ app/src/main/java/com/lpirro/tiledemo/MainActivity.kt
@@ -37,42 +37,11 @@ import androidx.appcompat.app.AppCompatActivity
 val REQUEST_CODE = 0;
 
 class MainActivity : AppCompatActivity() {
-    val mDPM: DevicePolicyManager by lazy {  getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager }
-    val  mAdminName: ComponentName by lazy { ComponentName(this, DeviceAdminDemo::class.java) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-
-
-        try {
-            if (!mDPM.isAdminActive(mAdminName)) {
-                val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
-                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mAdminName)
-                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Click on Activate button to secure your application.")
-                startActivityForResult(intent, REQUEST_CODE)
-            } else {
-                mDPM.setStatusBarDisabled(mAdminName, false)
-
-            }
-        }catch (e: Exception){
-            e.printStackTrace();
-        }
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (REQUEST_CODE === requestCode) {
-            if (requestCode == RESULT_OK) {
-                // done with activate to Device Admin
-                mDPM.setStatusBarDisabled(mAdminName, false)
-
-                block()
-            } else {
-                // cancle it.
-            }
-        }
+        block()
     }
 
     fun block() {
@@ -91,44 +60,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun preventStatusBarExpansion() {
-        val manager = applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        val LAYOUT_FLAG: Int
-        LAYOUT_FLAG = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY
-        } else {
-            WindowManager.LayoutParams.TYPE_PHONE
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode) {
+            100 -> blockStat()
         }
-
-        val layoutParam = WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                LAYOUT_FLAG,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT)
-        layoutParam.gravity = Gravity.TOP
-
-//
-//        val layoutParam = WindowManager.LayoutParams().also {
-//            it.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR
-//            it.gravity = Gravity.TOP
-//            it.flags = WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY
-//        }
-        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
-        layoutParam.width = WindowManager.LayoutParams.MATCH_PARENT
-        val resId = resources.getIdentifier("status_bar_height", "dimen", "android")
-        var result = 0
-        if (resId > 0) {
-            result = resources.getDimensionPixelSize(resId)
-        }
-        layoutParam.height = result;
-
-        layoutParam.format = PixelFormat.TRANSPARENT;
-
-
-        val  view =  CustomViewGroup(this);
-        manager.addView(view, layoutParam)
     }
+
+
 
     private fun blockStat() {
         val windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
--- app/src/main/java/com/lpirro/tiledemo/customquicksettings/CustomQuikSettingActivity.kt
+++ app/src/main/java/com/lpirro/tiledemo/customquicksettings/CustomQuikSettingActivity.kt
@@ -1,15 +1,23 @@
 package com.lpirro.tiledemo.customquicksettings
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ActivityManager
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.androidbolts.topsheet.TopSheetBehavior
-import com.androidbolts.topsheet.TopSheetDialog
+import com.lpirro.tiledemo.CustomViewGroup
 import com.lpirro.tiledemo.R
 import com.lpirro.tiledemo.databinding.ActivityCustomQuikSettingBinding
+
 
 class CustomQuikSettingActivity : AppCompatActivity() {
 
@@ -20,12 +28,13 @@ class CustomQuikSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCustomQuikSettingBinding.inflate(LayoutInflater.from(this))
-        setContentView(binding!!.root)
+//        setContentView(binding!!.root)
 
         binding?.customQuickSetting?.adapter = adapater
 
         val layoutManager = GridLayoutManager(this, 3)
         binding?.customQuickSetting?.layoutManager = layoutManager
+        binding?.customQuickSetting?.suppressLayout(true)
 
         adapater.setData(listOf(R.drawable.wifi_off,
                 R.drawable.wifi_on,
@@ -35,8 +44,60 @@ class CustomQuikSettingActivity : AppCompatActivity() {
                 R.drawable.airplanemode_active)
         )
 
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "Please give my app this permission!", Toast.LENGTH_SHORT).show();
+                val intent =  Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + packageName));
+                startActivityForResult(intent, 100);
+            } else {
+                blockStat()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode) {
+            100 -> {
+                blockStat()
+            }
+        }
+    }
+
+    fun addSystemOverlay() {
+        val params = WindowManager.LayoutParams(
+                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                PixelFormat.TRANSLUCENT)
+        params.gravity = Gravity.RIGHT or Gravity.TOP
+        params.title = "Load Average"
+        val wm = getSystemService(WINDOW_SERVICE) as WindowManager
         TopSheetBehavior.from(binding!!.topSheet).setState(TopSheetBehavior.STATE_EXPANDED);
 
+        wm.addView(binding!!.topSheet, params)
+    }
+
+    private fun blockStat() {
+        val windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+
+        val statusBarOverlay = CustomViewGroup(this)
+
+        val localLayoutParams = WindowManager.LayoutParams()
+
+        localLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        localLayoutParams.gravity = Gravity.TOP
+        localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or  // this is to enable the notification to receive touch events
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or  // Draws over status bar
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+
+        localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT
+        localLayoutParams.height = 300
+        localLayoutParams.format = PixelFormat.TRANSPARENT
+
+        windowManager.addView(binding!!.root, localLayoutParams)
     }
 
     override fun onDestroy() {
